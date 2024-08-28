13efa50f575f8358f1a41fedd2b1e28166ec0dd5
sumitpathak914 <sumitpathakofficial914@gmail.com>
2024-08-28 09:50:53 +0530

    working on changes

--- src/model/CommissionModel.js
+++ src/model/CommissionModel.js
@@ -4,12 +4,12 @@ const commissionSlabSchema = new mongoose.Schema({
     category: { type: String, required: true },
     subcategory: { type: String, required: true },
     commissions: {
-        dealer30Kg: { type: Number, required: true },
-        supplier30Kg: { type: Number, required: true },
-        dealer50Kg: { type: Number, required: true },
-        supplier50Kg: { type: Number, required: true },
-        dealer70Kg: { type: Number, required: true },
-        supplier70Kg: { type: Number, required: true },
+        dealer30Kg: { type: Number },
+        supplier30Kg: { type: Number },
+        dealer50Kg: { type: Number },
+        supplier50Kg: { type: Number },
+        dealer70Kg: { type: Number },
+        supplier70Kg: { type: Number },
     },
     isActive: { type: Boolean, default: true },
 });
