0f28523df27c3c7b22c096bfa55a33ee00123eea
sumitpathak914 <sumitpathakofficial914@gmail.com>
2024-11-21 12:57:24 +0530

    chanegs

--- src/routes/FactoryRoute.js
+++ src/routes/FactoryRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const FactoryController = require('../controllers/FactoryContoller'); // Adjust the path as per your project structure
 const authenticateToken = require('../middleware/auth');
 
-router.post('/AddFactory', authenticateToken, FactoryController.SaveFactory);
-router.get('/GetFactory', authenticateToken, FactoryController.getAllFactories);
+router.post('/AddFactory', FactoryController.SaveFactory);
+router.get('/GetFactory', FactoryController.getAllFactories);
 router.get('/FactoryCommission', FactoryController.calculateFactoryCommission);
 router.post('/generate-factory-invoice', FactoryController.generateFactoryInvoice);
 router.post('/GetFactorycommission', FactoryController.calculateSingleFactoryCommission);
