1db5e9be5902f60160fd908a7d857bc40e8c9ac2
sumitpathak914 <sumitpathakofficial914@gmail.com>
2024-11-21 13:51:58 +0530

    changes

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -4,8 +4,8 @@ const UserRouter = express.Router();
 const authenticateToken = require('../middleware/auth');
 const userController = require('../controllers/UserController');
 
-UserRouter.post('/GetFactories', authenticateToken, userController.getFactoriesByCategory);
-UserRouter.get('/GetAllFActories', authenticateToken, userController.getAllUsers);
+UserRouter.post('/GetFactories',  userController.getFactoriesByCategory);
+UserRouter.get('/GetAllFActories',  userController.getAllUsers);
 UserRouter.get('/factory/:factoryId', userController.getFactoryById);
 UserRouter.delete('/DeleteFactory/:id', userController.deleteFactory);
 UserRouter.put('/update-factory', userController.UpdateFactory);
